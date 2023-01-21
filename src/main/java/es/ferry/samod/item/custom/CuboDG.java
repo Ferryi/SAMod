8a37de098e98de05ba46895806bfbe8fcf68b796
Ferry21 <Pabloigi21@gmail.com>
2023-01-21 13:15:03 +0100

    Update CuboDG.java
    Ahora tiene una descripcion y si le damos al shift nos dice que hace.

--- src/main/java/es/ferry/samod/item/custom/CuboDG.java
+++ src/main/java/es/ferry/samod/item/custom/CuboDG.java
@@ -1,12 +1,18 @@
 package es.ferry.samod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CuboDG extends Item {
     public CuboDG(Properties properties) {
@@ -25,6 +31,18 @@ public class CuboDG extends Item {
 
         return super.use(level, player, hand);
     }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        if(Screen.hasShiftDown()){
+            components.add(Component.literal("Dale al Click derecho para saber de quien es este cubo").withStyle(ChatFormatting.AQUA));
+        }else{
+            components.add(Component.literal("Dale al shift para mas info").withStyle(ChatFormatting.BLUE));
+        }
+
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
+    }
+
     private void outputLinea(Player player){
         player.sendSystemMessage(Component.literal("Este cubo se ha llenado con lagrimas de DG14 jugando al LOL"));
     }
