1b777c9e1d493e1d2f510cd0a9f06922bba3ad0d
Ferry21 <Pabloigi21@gmail.com>
2023-01-25 15:02:43 +0100

    Actualizacion 1.19.3 completada
    FUNCIONA

---
+++ src/main/java/es/ferry/samod/datagen/ModWorldGenProvider.java
@@ -0,0 +1,24 @@
+package es.ferry.samod.datagen;
+
+import es.ferry.samod.SaMod;
+import es.ferry.samod.world.feature.ModConfiguredFeatures;
+import es.ferry.samod.world.feature.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Collections.singleton(SaMod.MOD_ID));
+    }
+}
